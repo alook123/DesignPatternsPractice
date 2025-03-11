bb1f3aec11634db1fd8239735d79abf923366aa5
alook <alook_6@live.com>
2025-03-11 22:25:52 +0800

    first commit

---
+++ DesignPatterns/SingletonPattern/Singleton.cs
@@ -0,0 +1,26 @@
+namespace DesignPatterns.SingletonPattern;
+
+// Singleton 类定义了 `GetInstance` 方法，它可以作为构造函数的替代，并允许客户端反复访问此类的相同实例。
+// EN：Singleton 应该始终是一个“密封”类，以防止通过外部类以及嵌套类进行类继承。
+public sealed class Singleton
+{
+    // Singleton 的构造函数应该始终是私有的，以防止使用“new”运算符直接构造调用。
+    private Singleton() { }
+
+    // Singleton 实例存储在静态字段中。有多种初始化此字段的方法，它们各有优缺点。在本例中，我们将展示这些方法中最简单的一种，然而，这种方法在多线程程序中实际上并不适用。
+    private static Singleton? _instance;
+
+    // 这是控制对Singleton实例访问的静态方法。在第一次运行时，它创建一个Singleton对象并将其放置到静态字段中。在随后的运行中，它返回存储在静态字段中的现有客户端对象。
+    public static Singleton GetInstance()
+    {
+        _instance ??= new Singleton();
+        return _instance;
+    }
+
+    // 最后，任何Singleton都应该定义一些业务逻辑，这些逻辑可以在其实例上执行。
+    public void SomeBusinessLogic()
+    {
+        // ...
+    }
+}
+
---
+++ DesignPatterns/SingletonPattern/ThreadSafeSingleton.cs
@@ -0,0 +1,35 @@
+namespace DesignPatterns.SingletonPattern;
+
+using System.Threading;
+
+// 这种 Singleton 实现被称为“双重检查锁”。它在多线程环境中是安全的，并为 Singleton 对象提供了延迟初始化。
+public sealed class ThreadSafeSingleton
+{
+    private ThreadSafeSingleton() { }
+
+    private static ThreadSafeSingleton? _instance;
+
+    // 我们现在有一个锁对象，它将用于在第一次访问 Singleton 时同步线程。
+    private static readonly Lock _lock = new();
+
+    public static ThreadSafeSingleton GetInstance(string value)
+    {
+        // 一旦实例准备就绪，就需要此条件来防止线程因锁而失败。
+        if (_instance == null)
+        {
+            // 现在，想象一下程序刚刚启动。由于还没有 Singleton 实例，多个线程可以同时通过前面的条件并几乎同时到达这一点。其中第一个将获取锁并继续执行，而其余线程将在此处等待。
+            lock (_lock)
+            {
+                // 第一个获取锁的线程到达此条件，进入内部并创建 Singleton 实例。一旦它离开锁块，可能一直在等待锁释放的线程可能会进入此部分。但由于 Singleton 字段已初始化，因此线程不会创建新对象。
+                _instance ??= new ThreadSafeSingleton
+                {
+                    Value = value
+                };
+            }
+        }
+        return _instance;
+    }
+
+    // 我们将使用此属性来证明我们的 Singleton 确实有效。
+    public string Value { get; set; } = null!;
+}
